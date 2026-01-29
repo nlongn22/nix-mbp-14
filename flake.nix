34dd5660a5cd6ddb771912c707f36dfa99f61bc6
nlongn22 <rebuke09puppies@icloud.com>
2026-01-29 20:10:30 +0100

    first commit

---
+++ flake.nix
@@ -0,0 +1,105 @@
+{
+  description = "MacBook Pro 14 nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      home-manager,
+      nixpkgs,
+      nix-homebrew,
+    }:
+    let
+      configuration =
+        { pkgs, ... }:
+        {
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = [
+            pkgs.vim
+            pkgs.nixd
+            pkgs.nixfmt
+          ];
+
+          homebrew = {
+            enable = true;
+            brews = [
+              "mas"
+            ];
+            casks = [
+              "ghostty"
+            ];
+            masApps = {
+              "wBlock" = 6746388723;
+            };
+            onActivation.cleanup = "zap";
+          };
+
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
+
+          # Enable alternative shell support in nix-darwin.
+          programs.fish.enable = true;
+          environment.shells = [ pkgs.fish ]; # Not working for me - had to switched manually.
+
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          system.primaryUser = "ngoclongnguyen";
+
+          users.users.ngoclongnguyen = {
+            home = "/Users/ngoclongnguyen";
+            shell = pkgs.fish; # Not working for me - had to switched manually.
+          };
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 6;
+
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
+        };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#mbp
+      darwinConfigurations."mbp" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.ngoclongnguyen = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              # Install Homebrew under the default prefix
+              enable = true;
+
+              # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+              enableRosetta = true;
+
+              # User owning the Homebrew prefix
+              user = "ngoclongnguyen";
+            };
+          }
+        ];
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,321 @@
+{ pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "ngoclongnguyen";
+  home.homeDirectory = "/Users/ngoclongnguyen";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "25.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    tig
+    nodejs_24
+  ];
+
+  xdg.configFile."fish/config.fish".text = ''
+    alias .. "cd .."
+    alias cd.. "cd .."
+    alias ll "ls -la"
+    alias gti git
+
+    function sudo
+      if test "$argv" = !!
+        eval command sudo $history[1]
+      else
+        command sudo $argv
+      end
+    end
+
+    starship init fish | source
+  '';
+
+  xdg.configFile."ghostty/config".text = ''
+    theme = dark:Gruvbox Dark Hard,light:Gruvbox Light Hard
+
+    font-thicken = true
+
+    window-padding-balance = true
+    window-padding-x = 10
+    window-padding-y = 10
+  '';
+
+  programs.starship = {
+    enable = true;
+  };
+
+  xdg.configFile."starship.toml".text = ''
+    "$schema" = 'https://starship.rs/config-schema.json'
+
+    format = """
+    [](color_orange)\
+    $os\
+    $username\
+    [](bg:color_yellow fg:color_orange)\
+    $directory\
+    [](fg:color_yellow bg:color_aqua)\
+    $git_branch\
+    $git_status\
+    [](fg:color_aqua bg:color_blue)\
+    $c\
+    $cpp\
+    $rust\
+    $golang\
+    $nodejs\
+    $php\
+    $java\
+    $kotlin\
+    $haskell\
+    $python\
+    [](fg:color_blue bg:color_bg3)\
+    $nix_shell\
+    [](fg:color_bg3 bg:color_bg1)\
+    $time\
+    [ ](fg:color_bg1)\
+    $line_break$character"""
+
+    palette = 'gruvbox_dark'
+
+    [palettes.gruvbox_dark]
+    color_fg0 = '#fbf1c7'
+    color_bg1 = '#3c3836'
+    color_bg3 = '#665c54'
+    color_blue = '#458588'
+    color_aqua = '#689d6a'
+    color_green = '#98971a'
+    color_orange = '#d65d0e'
+    color_purple = '#b16286'
+    color_red = '#cc241d'
+    color_yellow = '#d79921'
+
+    [os]
+    disabled = false
+    style = "bg:color_orange fg:color_fg0"
+
+    [os.symbols]
+    Windows = "󰍲"
+    Ubuntu = "󰕈"
+    SUSE = ""
+    Raspbian = "󰐿"
+    Mint = "󰣭"
+    Macos = "󰀵"
+    Manjaro = ""
+    Linux = "󰌽"
+    Gentoo = "󰣨"
+    Fedora = "󰣛"
+    Alpine = ""
+    Amazon = ""
+    Android = ""
+    AOSC = ""
+    Arch = "󰣇"
+    Artix = "󰣇"
+    EndeavourOS = ""
+    CentOS = ""
+    Debian = "󰣚"
+    Redhat = "󱄛"
+    RedHatEnterprise = "󱄛"
+    Pop = ""
+
+    [username]
+    show_always = true
+    style_user = "bg:color_orange fg:color_fg0"
+    style_root = "bg:color_orange fg:color_fg0"
+    format = '[ $user ]($style)'
+
+    [directory]
+    style = "fg:color_fg0 bg:color_yellow"
+    format = "[ $path ]($style)"
+    truncation_length = 3
+    truncation_symbol = "…/"
+
+    [directory.substitutions]
+    "Documents" = "󰈙 "
+    "Downloads" = " "
+    "Music" = "󰝚 "
+    "Pictures" = " "
+    "Developer" = "󰲋 "
+
+    [git_branch]
+    symbol = ""
+    style = "bg:color_aqua"
+    format = '[[ $symbol $branch ](fg:color_fg0 bg:color_aqua)]($style)'
+
+    [git_status]
+    style = "bg:color_aqua"
+    format = '[[($all_status$ahead_behind )](fg:color_fg0 bg:color_aqua)]($style)'
+
+    [nodejs]
+    symbol = ""
+    style = "bg:color_blue"
+    format = '[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)'
+
+    [c]
+    symbol = " "
+    style = "bg:color_blue"
+    format = '[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)'
+
+    [cpp]
+    symbol = " "
+    style = "bg:color_blue"
+    format = '[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)'
+
+    [rust]
+    symbol = ""
+    style = "bg:color_blue"
+    format = '[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)'
+
+    [golang]
+    symbol = ""
+    style = "bg:color_blue"
+    format = '[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)'
+
+    [php]
+    symbol = ""
+    style = "bg:color_blue"
+    format = '[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)'
+
+    [java]
+    symbol = ""
+    style = "bg:color_blue"
+    format = '[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)'
+
+    [kotlin]
+    symbol = ""
+    style = "bg:color_blue"
+    format = '[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)'
+
+    [haskell]
+    symbol = ""
+    style = "bg:color_blue"
+    format = '[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)'
+
+    [python]
+    symbol = ""
+    style = "bg:color_blue"
+    format = '[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)'
+
+    [nix_shell]
+    symbol = " "
+    style = "bg:color_bg3"
+    format = '[[ $symbol$state( \($name\)) ](fg:color_fg0 bg:color_bg3)]($style)'
+
+    [time]
+    disabled = false
+    time_format = "%R"
+    style = "bg:color_bg1"
+    format = '[[  $time ](fg:color_fg0 bg:color_bg1)]($style)'
+
+    [line_break]
+    disabled = false
+
+    [character]
+    disabled = false
+    success_symbol = '[](bold fg:color_green)'
+    error_symbol = '[](bold fg:color_red)'
+    vimcmd_symbol = '[](bold fg:color_green)'
+    vimcmd_replace_one_symbol = '[](bold fg:color_purple)'
+    vimcmd_replace_symbol = '[](bold fg:color_purple)'
+    vimcmd_visual_symbol = '[](bold fg:color_yellow)'
+  '';
+
+  programs.zed-editor = {
+    enable = true;
+
+    extensions = [
+      "nix"
+    ];
+
+    userSettings = {
+      ui_font_size = 16;
+      buffer_font_size = 15;
+
+      icon_theme = {
+        mode = "system";
+        light = "Zed (Default)";
+        dark = "Zed (Default)";
+      };
+      theme = {
+        mode = "system";
+        light = "Gruvbox Light Hard";
+        dark = "Gruvbox Dark Hard";
+      };
+
+      tabs = {
+        file_icons = true;
+        git_status = true;
+      };
+      title_bar = {
+        show_sign_in = false;
+        show_branch_icon = false;
+      };
+
+      autosave = "on_focus_change";
+      base_keymap = "VSCode";
+
+      vim = {
+        use_smartcase_find = true;
+        toggle_relative_line_numbers = true;
+      };
+      vim_mode = true;
+
+      git = {
+        inline_blame = {
+          delay_ms = 1000;
+        };
+      };
+
+      languages = {
+        Nix = {
+          language_servers = [
+            "nixd"
+            "!nil"
+          ];
+        };
+      };
+
+      telemetry = {
+        diagnostics = false;
+        metrics = false;
+      };
+
+      agent = {
+        default_model = {
+          provider = "copilot_chat";
+          model = "gpt-5-mini";
+        };
+        favorite_models = [ ];
+        model_parameters = [ ];
+      };
+      features = {
+        edit_prediction_provider = "copilot";
+      };
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name = "nlongn22";
+        email = "rebuke09puppies@icloud.com";
+      };
+      core = {
+        editor = "vim";
+      };
+      init = {
+        defaultBranch = "main";
+      };
+    };
+  };
+}
